c55a7235b2029a53b61fc8bf0b34b0988357e2a3
Samuel Eliab Granados Figuera <samuelgranados2801@gmail.com>
2024-08-01 20:45:45 -0300

    primer commit

---
+++ src/componentes/ImgPrincipal/ImgPrincipal.js
@@ -0,0 +1,11 @@
+import "./ImgPrincipal.css"
+
+function ImgPrincipal() {
+    return (
+        <header className="imagem">
+                <img src="/imagens/programadores.png" alt="Imagem principal da pagina, tem duas pessoas na frente de unos computadores" />
+        </header>
+    )
+}
+
+export default ImgPrincipal
